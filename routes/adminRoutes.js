03eb1afa1858fda0c2c120c468da06c047f89668
AryaK19 <aryakadam348@gmail.com>
2024-05-24 20:30:18 +0530

    added remove branch in database and updated the addbranch

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@
 
 import express from "express";
 import { addUser, editUser, removeUser } from "./userController.js";
-import { addBranch, editBranch, removeBranch, renderEditBranchPage,renderAddBranchPage,renderRemoveBranchPage } from "./branchController.js";
+import { addBranch, editBranch, removeBranch, renderEditBranchPage,renderAddBranchPage,renderRemoveBranchPage,getBranchesByCollege } from "./branchController.js";
 
 
 const router = express.Router();
@@ -12,14 +12,18 @@ router.post("/manage-users/add", addUser);
 router.put("/manage-users/edit", editUser);
 router.delete("/manage-users/remove", removeUser);
 
-// Branch Routes
-router.post('/manage-branches/add', addBranch);
-router.post('/manage-branches/edit', editBranch);
-router.post('/manage-branches/remove', removeBranch);
 
 // Route to render the edit branch page
 router.get('/manage-branches/add', renderAddBranchPage);
 router.get('/manage-branches/edit', renderEditBranchPage);
 router.get('/manage-branches/remove', renderRemoveBranchPage);
+
+// Branch Routes
+router.post('/manage-branches/add', addBranch);
+router.post('/manage-branches/edit', editBranch);
+router.post('/manage-branches/remove', removeBranch);
+
+router.get('/branches', getBranchesByCollege);
+
 
 export default router;
